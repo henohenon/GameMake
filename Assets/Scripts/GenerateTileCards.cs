8b2499a5cf454d7a26973a320fcc1bc963db5b21
henohenon <takeru.hawk.son@gmail.com>
2024-12-09 13:09:47 +0900

    generate tile cards

---
+++ Assets/Scripts/GenerateTileCards.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateTileCards : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject tileCardPrefab;
+    [SerializeField]
+    private int length = 9;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // xをlength回繰り返す
+        for(int x = 0; x < length; x++)
+        {
+            // zをlength回繰り返す
+            for (int z = 0; z < length; z++)
+            {
+                // タイルカードの座標を計算
+                var tileX = x - length / 2; // 真ん中のタイルが真ん中になるようにタイルの半分を引く
+                var tileZ = z - length / 2;
+                var tileVector = new Vector3(tileX, 0, tileZ);
+                var tileQuaternion = Quaternion.identity; // 回転なし、0度の状態
+                // タイルカードを生成
+                GameObject tileCard = Instantiate(tileCardPrefab, tileVector, tileQuaternion); 
+            }
+        }
+    }
+}

cc21cfa6cc9bc88ad2b4d3f826a3404bb874cf43
henohenon <takeru.hawk.son@gmail.com>
2024-12-09 14:24:00 +0900

    remove on space

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -29,5 +29,18 @@ public class PlayerController : MonoBehaviour
         // 移動と回転
         transform.position += movement;
         transform.Rotate(rotation);
+        
+        // スペースキーを押したら
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            RaycastHit hit;
+            // 自分の位置から下方向に2のRayを飛ばす
+            if(Physics.Raycast(transform.position, -transform.up, out hit, 2))
+            {
+                // 当たったやつをxxする
+                var hitObject = hit.collider.gameObject;
+                Destroy(hitObject);
+            }
+        }
     }
 }

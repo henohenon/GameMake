8ef1d0480655937892b1a6bb6a3b2973f80b2ffb
pppget <nakasa7001@gmail.com>
2024-12-10 16:35:46 +0900

    色々

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,41 +14,54 @@ public class PlayerController : MonoBehaviour//へのへのさん
     private InputActionReference space;
     [SerializeField]
     private AudioManager audioManager;
+    [SerializeField]
+    private InputActionReference tree;
+    [SerializeField]
+    private InputActionReference four;
+    [SerializeField] 
+    private Camera subcamera;
+    [SerializeField]
+    private Transform alternateTarget; // 別のオブジェクトの参照を追加
 
     private Rigidbody _rb;
-    
+
     private void Start()
     {
         _rb = GetComponent<Rigidbody>();
         space.action.performed += _ => audioManager.SetSE1(); //キャンセルとかもある
         space.action.Enable();
+
+        tree.action.performed += _ => rotcam1(); //キャンセルとかもある
+        tree.action.Enable();
+        four.action.performed += _ => rotcam2(); //キャンセルとかもある
+        four.action.Enable();
     }
-    
+
     private void Update()
     {
         // キー入力を取得
         var horizontal = Input.GetAxis("Horizontal");
         var vertical = Input.GetAxis("Vertical");
-        
+
         // 移動方向を計算
         var movement = transform.forward * vertical; // 正面に対して前後の入力
-        
+
         // 回転方向を計算
         var rotation = Vector3.up * horizontal; // 上方向に対して左右の入力
-        
+
         // スピードとフレームの経過時間をかける
         movement *= moveSpeed * Time.deltaTime;
         rotation *= rotateSpeed * Time.deltaTime;
-        
+
         // 移動と回転
         _rb.position += movement;
-        _rb.MoveRotation(_rb.rotation * Quaternion.Euler(rotation));        
+        _rb.MoveRotation(_rb.rotation * Quaternion.Euler(rotation));
         // スペースキーを押したら
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             RaycastHit hit;
             // 自分の位置から下方向に2のRayを飛ばす
-            if(Physics.Raycast(transform.position, -transform.up, out hit, 2))
+            if (Physics.Raycast(transform.position, -transform.up, out hit, 2))
             {
                 // Rayが当たったオブジェクトを取得
                 var hitObject = hit.collider.gameObject;
@@ -61,7 +74,7 @@ public class PlayerController : MonoBehaviour//へのへのさん
             }
         }
     }
-    
+
     public void Impact(Vector3 direction)
     {
         //ここに爆発のSEを入れたい
@@ -71,5 +84,24 @@ public class PlayerController : MonoBehaviour//へのへのさん
         _rb.AddForce(direction * 10, ForceMode.Impulse);
         Vector3 torqueAxis = Vector3.Cross(direction, Vector3.up); // 適当にgptに吐かせた。なにやってるのかわかってない
         _rb.AddTorque(torqueAxis * 10, ForceMode.Impulse);
+        // カメラのターゲットを変更
+        ChangeCameraTarget(alternateTarget);
+    }
+        private void ChangeCameraTarget(Transform newTarget)
+    {
+        // カメラのターゲットを新しいターゲットに変更
+        subcamera.transform.parent = newTarget;
+        subcamera.transform.localPosition = Vector3.zero;
+        subcamera.transform.localRotation = Quaternion.identity;
+    }
+
+    void rotcam1()
+    {
+        Debug.Log("rotcam1");
+    }
+    void rotcam2()
+    {
+        Debug.Log("rotcam2");
     }
 }
+

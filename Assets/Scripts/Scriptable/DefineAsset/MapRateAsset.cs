42d6bf65e501631bce40ef2efb86fd9189deeabd
henohenon <takeru.hawk.son@gmail.com>
2025-01-03 08:19:34 +0900

    update data

--- Assets/Scripts/Scriptable/DefineAsset/MapRateAsset.cs
+++ Assets/Scripts/Scriptable/DefineAsset/MapRateAsset.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 namespace Scriptable
 {
     // クラスはserializableをつけるとインスペクターでいじれるようになる(シリアル化可能な変数で構成されている場合に限る)
-    [Serializable]
+    [CreateAssetMenu(fileName = "MapRateData", menuName = "MapRateData")]
     public class MapRateAsset: ScriptableObject
     {
         [AssetsOnly]

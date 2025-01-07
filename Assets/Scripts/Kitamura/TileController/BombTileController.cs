b3620a9ab2847a77961d5d4eb69615157b4fb4c0
henohenon <takeru.hawk.son@gmail.com>
2025-01-07 23:44:13 +0900

    add dead sound

--- Assets/Scripts/Kitamura/TileController/BombTileController.cs
+++ Assets/Scripts/Kitamura/TileController/BombTileController.cs
@@ -1,13 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Serialization;
 
+[RequireComponent(typeof(AudioSource))]
 public class BombTileController : TileController
 {
     [SerializeField] private GameObject bombObject;
-    [SerializeField] private Collider damageCollider; 
+    [SerializeField] private Collider damageCollider;
+    [SerializeField] private AudioClip alertAudio;
+    [SerializeField] private AudioClip explosionAudio;
     
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
     public override bool Open()
     {
         var baseResult =  base.Open();
@@ -22,8 +34,23 @@ public class BombTileController : TileController
 
     private async void Explosion()
     {
-        await UniTask.WaitForSeconds(0.3f);
+        if (_audioSource)
+        {
+            _audioSource.clip = alertAudio;
+            _audioSource.loop = true;
+            _audioSource.time = 0.25f;
+            _audioSource.pitch = 2;
+            _audioSource.Play();
+        }
+        await UniTask.WaitForSeconds(0.5f);
         if(damageCollider) damageCollider.enabled = true;
+        if (_audioSource)
+        {
+            _audioSource.clip = explosionAudio;
+            _audioSource.loop = false;
+            _audioSource.pitch = 1;
+            _audioSource.Play();
+        }
 
         await UniTask.WaitForSeconds(0.1f);
         if(damageCollider) damageCollider.enabled = false;

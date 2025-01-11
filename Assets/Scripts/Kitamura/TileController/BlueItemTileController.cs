1633c4d9163affd17769fa61ca23296750c60b51
henohenon <66393463+henohenon@users.noreply.github.com>
2025-01-11 12:37:41 +0900

    add item 3d view

--- Assets/Scripts/Kitamura/TileController/BlueItemTileController.cs
+++ Assets/Scripts/Kitamura/TileController/BlueItemTileController.cs
@@ -5,26 +5,42 @@ using UnityEngine;
 
 public class BlueItemTileController : TileController
 {
-    [SerializeField]
-    private GameObject blueObject;
-    [SerializeField] private TextMesh itemText;
-
-    private Sprite _itemIcon;
+    [SerializeField] private MeshRenderer iconRenderer;
 
     public void SetItemIcon(Sprite itemIcon)
     {
-        _itemIcon = itemIcon;
-        // itemText.text = _itemIcon;
-    }
+        // SpriteからTexture2Dを取得する
+        Texture2D texture = itemIcon.texture;
+        iconRenderer.material.mainTexture = texture;
+        
+        // テクスチャの幅と高さを取得
+        float width = texture.width;
+        float height = texture.height;
+
+        // オブジェクトのスケールを調整
+        Vector3 newScale = iconRenderer.transform.localScale;
+
+        if (width > height)
+        {
+            // 横長の場合、横を基準にスケールを調整
+            newScale.y = newScale.x * (height / width);
+        }
+        else
+        {
+            // 縦長の場合、縦を基準にスケールを調整
+            newScale.x = newScale.y * (width / height);
+        }
+
+        // スケールを適用
+        iconRenderer.transform.localScale = newScale;
+   }
 
     public override bool Open()
     {
         var baseResult = base.Open();
         if (baseResult)
         {
-            blueObject.SetActive(true);
-            itemText.gameObject.SetActive(true);
-            Debug.Log(_itemIcon);
+            iconRenderer.gameObject.SetActive(true);
         }
 
         return baseResult;

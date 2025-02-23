344d5842f52d228df38f321ad4ec73d30b479ec1
henohenon <66393463+henohenon@users.noreply.github.com>
2025-02-23 18:50:29 +0900

    fix build error

--- Assets/Scripts/nikomaru/TitleUIManager.cs
+++ Assets/Scripts/nikomaru/TitleUIManager.cs
@@ -1,11 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem.iOS;
 using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-using Button = UnityEngine.UIElements.Button;
 
 
 /// <summary>
